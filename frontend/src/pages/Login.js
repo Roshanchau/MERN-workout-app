d9c10e07af6373d44d00492b204abef8e900e48b
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-05-23 16:21:57 +0545

    setting the Initial Auth  status

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,18 +5,17 @@ import { useLogin } from "../hooks/useLogin";
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const{login , error , isLoading}=useLogin();
+  const { login, error, isLoading } = useLogin();
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await login(email, password)
+    await login(email, password);
   };
 
   return (
     <form className="login" onSubmit={handleSubmit}>
       <h3>Log in</h3>
-
       <label>Email:</label>
       <input
         type="email"
@@ -29,9 +28,9 @@ const Login = () => {
         onChange={(e) => setPassword(e.target.value)}
         value={password}
       />
-
+      {/* //log in button is disabled if we are writing the email/password */}
       <button disabled={isLoading}>Log in</button>
-      {error&& <div className="error">{error}</div>}
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
