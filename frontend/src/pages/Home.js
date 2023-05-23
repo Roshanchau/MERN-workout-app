4fe7dc0df10ba02d6c10f2de591e6f369952da8d
Roshan Chaudhary <87389059+Roshanchau@users.noreply.github.com>
2023-05-23 20:09:11 +0545

    making authorized requests

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 //components
 import WorkoutDetails from "../components/WorkoutDetails";
@@ -8,18 +9,26 @@ import WorkoutForm from "../components/WorkoutForm";
 const Home = () => {
   //instead of using a local state we are using a  global context.
   const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts");
+      const response = await fetch("/api/workouts" , {
+        headers:{
+            "Authorization": `Bearer ${user.token}`
+        }
+      }); 
       const json = await response.json(); // we parse the workout response and get an array of workout objects.
       if (response.ok) {
         //after getting the response we fire the dispatch function which invokes the workoutsReducer function in the workoutContext.js and passes the passes in the action which is the (type:"same as below" , payload:is the json which is the whole response while fetching the data. )
         dispatch({ type: "SET_WORKOUTS", payload: json });
       }
     };
-    fetchWorkouts();
-  }, [dispatch]); //we use an empty array so that the effect hook works only one time
+
+    if (user) {
+      fetchWorkouts();
+    }
+  }, [dispatch , user]); //we use an empty array so that the effect hook works only one time
 
   return (
     <>
@@ -28,7 +37,7 @@ const Home = () => {
           {workouts &&
             workouts.map((workout) => (
               <WorkoutDetails key={workout._id} workout={workout} />
-            ))} 
+            ))}
         </div>
         <WorkoutForm />
       </div>
